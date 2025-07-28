6d9004db5746f598490598e5574b2b96a7eb050b
Ramlakhan <sspl.ramlakhan2023@gmail.com>
2025-07-28 12:49:59 +0530

    Changed the text of copyright in footer component, changed the logo.

--- todoapp.client/src/Components/AppFooter/AppFooter.jsx
+++ todoapp.client/src/Components/AppFooter/AppFooter.jsx
@@ -5,13 +5,9 @@ const AppFooter = () => {
     return (
         <CFooter className="px-4">
             <div>
-                <span className="ms-1">&copy; 2025 by Ramlakhan Saini.</span>
-            </div>
-            <div className="ms-auto">
-                <span className="me-1">Powered by</span>
-                <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-                    CoreUI React Admin &amp; Dashboard Template
-                </a>
+                <span className="ms-1">
+                    &copy; {new Date().getFullYear()} ToDoApp. All rights reserved.
+                </span>
             </div>
         </CFooter>
     )
